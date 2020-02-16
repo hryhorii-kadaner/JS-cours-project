a1695e31d346bc762fdc6d3e8426eadd9577897a
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-02-16 12:13:48 +0200

    Home work #5.1

---
+++ Home work 5.1/js/js.js
@@ -0,0 +1,43 @@
+var startCalculating = document.getElementById('start'),
+    resultTable = document.querySelectorAll('.result-table'),
+    budgetValue = document.querySelector('.budget-value'),
+    daybudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalexpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthsavingsValue = document.querySelector('.monthsavings-value'),
+    yearsavingsValue = document.querySelector('.yearsavings-value'),
+    expensesItems = document.getElementsByClassName('expenses-item'),
+    submitFirst = document.getElementsByTagName('button')[0],
+    submitSecond = document.getElementsByTagName('button')[2],
+    optionalexpensesItems = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('.checksavings input'),
+    chooseSum = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+console.log(startCalculating);
+console.log(resultTable);
+console.log(budgetValue);
+console.log(daybudgetValue);
+console.log(levelValue);
+console.log(expensesValue);
+console.log(optionalexpensesValue);
+console.log(incomeValue);
+console.log(monthsavingsValue);
+console.log(yearsavingsValue);
+console.log(expensesItems);
+console.log(submitFirst);
+console.log(submitSecond);
+console.log(optionalexpensesItems);
+console.log(chooseIncome);
+console.log(checkSavings);
+console.log(chooseSum);
+console.log(choosePercent);
+console.log(yearValue);
+console.log(monthValue);
+console.log(dayValue);
