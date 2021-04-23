27308459ac86ee480e279b8f93cd2ebec3f3a42a
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2021-04-23 14:19:43 +0300

    38. Практика
    Создаю табы в новом проекте

---
+++ New JS course/38. Создаем табы в новом проекте/js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', function () {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(content => {
+            content.classList.add('hide');
+            content.classList.remove('show', 'fade');
+        });
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
